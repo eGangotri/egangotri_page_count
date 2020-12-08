59de0f5fd3faf9bf2efeaea725b91af5199a40e2
eGangotri <egangotritrust@gmail.com>
2020-12-09 00:51:29 +0800

    '${comment:-Commit-eGangotri-Page-Counter}'

--- src/app/utils/Utils.ts
+++ src/app/utils/Utils.ts
@@ -1,15 +1,21 @@
-export function sizeInfo(sizeInKB: number) {
-    const sizeInMB = sizeInKB / 1024
-    if(sizeInMB >= 1024){
+export function sizeInfo(sizeInBytes: number) {
+    const sizeInKB = sizeInBytes /1024
+    const sizeInMB = sizeInKB / 1024;
+
+    if(sizeInKB < 1024){
+        return `${roundOff(sizeInKB)} KB`;
+    }
+
+    else if(sizeInMB >= 1024){
         const sizeInGB = sizeInMB / 1024;
         return `${roundOff(sizeInGB)} GB`;
     }
-    if(sizeInKB >= 1024){
+    else{
         return `${roundOff(sizeInMB)} MB`;
     }
-    return `${roundOff(sizeInKB)} KB`;
 }
 
 export function roundOff(num:number){
-    return Math.round((num + Number.EPSILON) * 100) / 100
+    return Math.round((num + Number.EPSILON) * 100) / 100;
 }
+
